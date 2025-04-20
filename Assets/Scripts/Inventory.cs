a830b97ff75178e2882567b2934993388f885b96
aleksandraizmaylova <aleksandraizmaylova111@gmail.com>
2025-04-20 22:49:16 +0500

    Инвентарь доделан

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -2,6 +2,61 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public bool[] IsSlotFull;
+    public bool[] isSlotFull;
+    public int selectedSlot;
     public GameObject[] Slots;
+    public GameObject openedBP;
+    public GameObject closedBP;
+    private bool InventoryOpen;
+
+    private void Start()
+    {
+        selectedSlot = 0;
+        InventoryOpen = false;
+        openedBP.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // открытие/закрытие инвентаря
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            InventoryOpen = !InventoryOpen;
+            openedBP.SetActive(InventoryOpen);
+            closedBP.SetActive(!InventoryOpen);
+        }
+        
+        // смена активного слота
+        if (InventoryOpen)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Slots[selectedSlot].GetComponent<Slot>().Unselect();
+                selectedSlot = 0;
+                Slots[selectedSlot].GetComponent<Slot>().Select();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                Slots[selectedSlot].GetComponent<Slot>().Unselect();
+                selectedSlot = 1;
+                Slots[selectedSlot].GetComponent<Slot>().Select();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                Slots[selectedSlot].GetComponent<Slot>().Unselect();
+                selectedSlot = 2;
+                Slots[selectedSlot].GetComponent<Slot>().Select();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                Slots[selectedSlot].GetComponent<Slot>().Unselect();
+                selectedSlot = 3;
+                Slots[selectedSlot].GetComponent<Slot>().Select();
+            }
+        }
+        
+    }
 }
--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,30 +1,27 @@
-using System;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    private Inventory inventory;
+    private Inventory Inventory;
     public GameObject slotButton;
 
     private void Start()
     {
-
-        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        Inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
     
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player") && Input.GetKey(KeyCode.E))
         {
-            for (var i = 0; i < inventory.Slots.Length; i++)
+            for (var i = 0; i < Inventory.Slots.Length; i++)
             {
-                if (inventory.IsSlotFull[i] == false)
-                {
-                    inventory.IsSlotFull[i] = true;
-                    Instantiate(slotButton, inventory.Slots[i].transform);
-                    Destroy(gameObject);
-                    break;
-                }
+                if (Inventory.isSlotFull[i])
+                    continue;
+                Inventory.isSlotFull[i] = true;
+                Instantiate(slotButton, Inventory.Slots[i].transform);
+                Destroy(gameObject);
+                break;
             }
         }
     }
