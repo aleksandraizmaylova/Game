4cfaf6164d97f563c4461123466f5c490bf4f927
Ippppppppat <GimDaniil@gmail.com>
2025-04-21 18:01:04 +0500

    анимация

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,35 +2,76 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	private Rigidbody2D rb2;
-	
-	private float currentSpeed;
-	
-	private void Awake()
-	{
-		rb2 = GetComponent<Rigidbody2D>();
-		rb2.freezeRotation = true;
-		currentSpeed = Constants.NormalSpeed;
-	}
-	
-	private void Update()
-	{
-		ChangeSpeed();
-	}
+    public static Player Instance { get; private set; }
 
-	private void FixedUpdate()
-	{
-		var movementVector = GameInput.Instance.GetMovementVector();
-		rb2.linearVelocity = movementVector * currentSpeed;
-	}
+    private Rigidbody2D rb2;
 
-	private void ChangeSpeed()
-	{
-		if (GameInput.Instance.IsSneaking())
-			currentSpeed = Constants.SneakySpeed;
-		else if (GameInput.Instance.IsSprinting())
-			currentSpeed = Constants.SprintSpeed;
-		else
-			currentSpeed = Constants.NormalSpeed;
-	}
+    private float currentSpeed;
+
+    private float minMovingSpeed = 0.1f;
+
+    private bool isRunningW = false;
+    private bool isRunningA = false;
+    private bool isRunningS = false;
+    private bool isRunningD = false;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        rb2 = GetComponent<Rigidbody2D>();
+        rb2.freezeRotation = true;
+        currentSpeed = Constants.NormalSpeed;
+    }
+
+    private void Update() => ChangeSpeed();
+
+    private void FixedUpdate() => HandleMovement();
+
+    private void ChangeSpeed()
+    {
+        if (GameInput.Instance.IsSneaking())
+            currentSpeed = Constants.SneakySpeed;
+        else if (GameInput.Instance.IsSprinting())
+            currentSpeed = Constants.SprintSpeed;
+        else
+            currentSpeed = Constants.NormalSpeed;
+    }
+
+    private void HandleMovement()
+    {
+        var movementVector = GameInput.Instance.GetMovementVector();
+        rb2.linearVelocity = movementVector * currentSpeed;
+
+        if (Mathf.Abs(movementVector.x) > minMovingSpeed || Mathf.Abs(movementVector.y) > minMovingSpeed)
+        {
+            // —начала провер€ем вертикальное движение (имеет приоритет)
+            if (Mathf.Abs(movementVector.y) > minMovingSpeed)
+            {
+                isRunningW = movementVector.y > 0;
+                isRunningS = movementVector.y < 0;
+                // ≈сли движемс€ вверх/вниз, игнорируем горизонтальные направлени€
+                isRunningA = false;
+                isRunningD = false;
+            }
+            else
+            {
+                // ≈сли нет вертикального движени€, провер€ем горизонтальное
+                isRunningD = movementVector.x > 0;
+                isRunningA = movementVector.x < 0;
+            }
+        }
+        else
+        {
+            isRunningW = false;
+            isRunningA = false;
+            isRunningS = false;
+            isRunningD = false;
+        }
+    }
+
+    public bool IsRunningW() => isRunningW;
+    public bool IsRunningA() => isRunningA;
+    public bool IsRunningS() => isRunningS;
+    public bool IsRunningD() => isRunningD;
 }
