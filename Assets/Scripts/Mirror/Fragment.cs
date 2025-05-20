1e414ee9e4c117c58ce4f6869aae10bd7b491abe
anyanyan3 <annpavlova06@gmail.com>
2025-05-20 19:45:33 +0300

    добавила 1 катсцену (и спрайты медсестры)

--- Assets/Scripts/Mirror/Fragment.cs
+++ Assets/Scripts/Mirror/Fragment.cs
@@ -1,21 +1,35 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class Fragment : MonoBehaviour
 {
+    public GameObject cutscene;
     private Mirror Mirror;
     public int fragmentNumber;
+
+    private VideoPlayer videoCutscene;
 
     private void Start()
     {
         Mirror = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>().mirror;
+        videoCutscene = cutscene.GetComponent<VideoPlayer>();
+        videoCutscene.loopPointReached += OnVideoFinished;
+        videoCutscene.isLooping = false;
     }
-    
-    private void OnTriggerStay2D(Collider2D other)
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            cutscene.SetActive(true);
+            videoCutscene.Play();
             Mirror.ActivateFragment(fragmentNumber);
             Destroy(gameObject);
         }
+    }
+
+    private void OnVideoFinished(VideoPlayer vp)
+    {
+        cutscene.SetActive(false);
     }
 }
