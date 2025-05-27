089e4e37cf7e0f939eee19740be604fccdd72683
aleksandraizmaylova <aleksandraizmaylova111@gmail.com>
2025-05-27 15:46:47 +0500

    Еще поделала всякое

--- Assets/Scripts/Phone.cs
+++ Assets/Scripts/Phone.cs
@@ -1,15 +1,27 @@
 using System;
 using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Phone : MonoBehaviour
 {
+    [Header("Phone Settings")]
     public Animator animator;
+    public int phoneNumber = -1;
     public GameObject newCanvas;
     public GameObject oldCanvas;
-    public int phoneNumber = -1;
-    public GameObject phone;
     public GameObject fragment;
+    [Space]
+    [Header("Dialog Settings")]
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    [Space]
+    [Header("Teleport Settings")]
+    public CinemachineCamera closeUpCamera;
+    public CinemachineCamera longShotCamera;
+    public GameObject phone;
+    
     private int result;
     private Player player;
     private Vector2 position;
@@ -40,20 +52,25 @@ public class Phone : MonoBehaviour
         }
     }
 
-    private void Update()
+    public void OnAnimationEnding()
     {
+        dialoguePanel.SetActive(false);
         if (result == phoneNumber)
         {
             fragment.SetActive(true);
             phone.SetActive(false);
             result = -1;
-            // player.transform.position = position;
+            closeUpCamera.enabled = false;
+            longShotCamera.enabled = true;
+            player.transform.position = position;
         }
     }
 
     public void Push(int i)
     {
         animator.SetTrigger($"{i}");
+        dialoguePanel.SetActive(true);
+        dialogueText.text = $"{i}...";
         result = result * 10 + i;
         Debug.Log($"result: {result}");
     }
