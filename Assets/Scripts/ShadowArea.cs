16c263a63848ea8f090642a877a07d933273b4c0
aleksandraizmaylova <aleksandraizmaylova111@gmail.com>
2025-06-01 15:34:38 +0500

    Добавила обратный отсчет и автотелепортацию

--- Assets/Scripts/ShadowArea.cs
+++ Assets/Scripts/ShadowArea.cs
@@ -1,16 +1,45 @@
 using System;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class ShadowArea : MonoBehaviour
 {
+    public float timer;
+    public GameObject teleporter;
+    public GameObject panel;
+    public Text text;
+    
     private Light2D playerLight;
     private Light2D globalLight;
+    private Player player;
     
     private void Start()
     {
+        player = Player.Instance;
         globalLight = GameObject.FindGameObjectWithTag("GlobalLight").GetComponent<Light2D>();
-        playerLight = Player.Instance.GetComponent<Light2D>();
+        playerLight = player.GetComponent<Light2D>();
+    }
+
+    private void Update()
+    {
+        if (!player.canMove)
+            return;
+        if (timer < 5)
+        {
+            panel.SetActive(true);
+            text.text = "Что-то мне нехорошо...";
+            player.ChangeSpeed(2f);
+            playerLight.pointLightOuterRadius = 1.5f;
+        }
+        if (timer > 0)
+            timer -= Time.deltaTime;
+        else
+        {
+            panel.SetActive(false);
+            teleporter.SetActive(true);
+            player.ChangeSpeed(Constants.NormalSpeed);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
